4d902dad752517d89102e8214e2158a76385d9c0
Hikaru Egashira <shighten@gmail.com>
2019-01-05 09:49:47 +0900

    first commit

---
+++ src/components/homebody.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container>
+    <v-layout wrap>
+      <v-flex mb-5 xs12>
+        <h2 class="headline font-weight-bold mb-3">これはなに？</h2>
+        <v-card hover>
+          <v-card-text>
+            <v-card-text
+              class="subheading font-weight-regular"
+            >ITF Attendance Manager (ITF.AM) は、筑波大学での出席日数を記録して
+              <br>いつ休んだのかを確認できるようにするアプリです。
+            </v-card-text>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs12 mb-5>
+        <h2 class="headline font-weight-bold mb-3">つかいかた</h2>
+
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({})
+};
+</script>
+
+<style>
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <v-parallax dark src="https://cdn.vuetifyjs.com/images/parallax/material.jpg">
+      <v-layout align-center column justify-center>
+        <h1 class="display-2 font-weight-thin mb-3">ITF Attendance Manager</h1>
+        <v-btn class="mt-4" depressed large color="white" href="settings">getting start</v-btn>
+        <v-btn
+          outline
+          depressed
+          large
+          color="white"
+          href="https://community.vuetifyjs.com"
+          target="_blank"
+        >Github Page</v-btn>
+      </v-layout>
+    </v-parallax>
+    <homebody/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import homebody from "@/components/homebody.vue"; // @ is an alias to /src
+
+@Component({
+  components: {
+    homebody
+  }
+})
+export default class Home extends Vue {}
+</script>
